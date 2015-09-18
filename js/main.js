9e0445fdd786b42c01bd70c0885b2fd9e5f025a1
Zack Breakstone <z.breakstone@gmail.com>
2015-09-18 14:01:01 -0400

    Form Test for customer portal

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+	$('body').colorScroll({
+        colors: [{
+            color: '#E4572E',
+            position: 1000
+        }, {
+            color: '#FBFBFB',
+            position: 3000
+        }, {
+            color: '#F0F66E',
+            position: 5000
+        }, {
+            color: '#63D2FF',
+            position: 7000
+        }, {
+            color: '#FFFFFF',
+            position: 9000
+        }]
+    });
+});
