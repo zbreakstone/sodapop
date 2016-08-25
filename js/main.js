1b224a946ca4a54c1a99e3d588b3fe61e282920a
Zack Breakstone <z.breakstone@gmail.com>
2016-08-25 17:53:14 -0400

    Add additional parameters to hs_context

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 $(function() {
   // Get all cookies from current document
-  var cookies = document.cookie.split('; ');
+  var cookies = document.cookie.split('; '),
+      pageURL = window.location.href,
+      pageName = document.title,
+      redirectURL = "http://www.thoughtfire.us/sodapop/";
 
   // Create function to parse out and return HubSpot User Token value
   var getUserToken = function(arr) {
@@ -14,7 +17,10 @@ $(function() {
 
   // Create JavaScript object to send as payload
   var hs_context = {
-    hutk: getUserToken(cookies) // Runs function and sets result as the value of the hutk property
+    hutk: getUserToken(cookies), // Runs function and sets result as the value of the hutk property
+    pageURL: pageURL,
+    pageName: pageName,
+    redirectURl: redirectURL;
   };
 
   var contextString = JSON.stringify(hs_context); // Converts payload to JSON string
