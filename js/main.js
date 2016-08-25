298de8157b292796eeb5d5b156a5da4a790f78af
Zack Breakstone <z.breakstone@gmail.com>
2016-08-25 16:49:48 -0400

    Fix url in HS Analytics code

--- js/main.js
+++ js/main.js
@@ -1,42 +1,21 @@
 $(function() {
-  //var frm = $(document.offer);
-  //var dat = JSON.stringify(frm.serializeArray());
-  //
-
+  // Get all cookies from current document
   var cookies = document.cookie.split('; ');
 
-  // for (var i = 0; i < cookies.length; i++) {
-  //   var c = cookies[i].split('=');
-  //   c[0] === 'hubspotutk' ? hutk.push(c[1]) : '';
-  // }
-
+  // Create function to parse out and return HubSpot User Token value
   var getUserToken = function(arr) {
     for (var i = 0, c = arr[i].split('='); i < arr.length; i++) {
       return c[0] === 'hubspotutk' ? c[1] : '';
     }
   };
 
+  // Create JavaScript object to send as payload
   var hs_context = {
-    hutk: getUserToken(cookies)
+    hutk: getUserToken(cookies) // Runs function and sets result as the value of the hutk property
   };
 
-  var contextString = JSON.stringify(hs_context);
+  var contextString = JSON.stringify(hs_context); // Converts payload to JSON string
 
+  // Set the JSON string as the value for the hs_context input
   $('input[name="hs_context"]').val(contextString);
 });
-
-// $('#myform').submit(function(e) {
-//   $.ajax({
-//     datatype: "json",
-//     contentType: "application/x-www-form-urlencoded",
-//     type: "POST",
-//     url: 'https://forms.hubspot.com/uploads/form/v2/321750/5e525fa8-5c62-4090-af25-adb04d1fb20d',
-//     data: JSON.stringify(dat),
-//     success: function() {},
-//     error: function() {},
-//   });
-//   // Stops browser refresh
-//   e.preventDefault();
-// });
-// // Submit on document ready
-// $('#offer').submit();
