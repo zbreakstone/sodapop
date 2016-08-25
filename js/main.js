3ae8fd86f91da784b28d8009678d3dc1a7e15382
Zack Breakstone <z.breakstone@gmail.com>
2016-08-25 14:44:13 -0400

    Add API form to test for Kait Scott

--- js/main.js
+++ js/main.js
@@ -1,20 +1,48 @@
-$(document).ready(function(){
-	$('body').colorScroll({
-        colors: [{
-            color: '#E4572E',
-            position: 1000
-        }, {
-            color: '#FBFBFB',
-            position: 3000
-        }, {
-            color: '#F0F66E',
-            position: 5000
-        }, {
-            color: '#63D2FF',
-            position: 7000
-        }, {
-            color: '#FFFFFF',
-            position: 9000
-        }]
+// $(document).ready(function(){
+//  $('body').colorScroll({
+//         colors: [{
+//             color: '#E4572E',
+//             position: 1000
+//         }, {
+//             color: '#FBFBFB',
+//             position: 3000
+//         }, {
+//             color: '#F0F66E',
+//             position: 5000
+//         }, {
+//             color: '#63D2FF',
+//             position: 7000
+//         }, {
+//             color: '#FFFFFF',
+//             position: 9000
+//         }]
+//     });
+// });
+
+$(function() {
+  //var frm = $(document.offer);
+  //var dat = JSON.stringify(frm.serializeArray());
+  var dat = {
+    firstName: $('#firstName').val(),
+    lastName: $('#lastName').val(),
+    email: $('#email').val(),
+    industry: $('#industry').val(),
+    current_persona: $('#current_persona').val()
+  }
+
+  $('#offer').submit(function(e) {
+    $.ajax({
+      datatype: "json",
+      contentType: "application/x-www-form-urlencoded",
+      type: "POST",
+      url: 'https://forms.hubspot.com/uploads/form/v2/321750/5e525fa8-5c62-4090-af25-adb04d1fb20d',
+      data: JSON.stringify(dat),
+      success: function() {},
+      error: function() {},
     });
+    // Stops browser refresh
+    e.preventDefault();
+  });
+  // Submit on document ready
+  // $('#offer').submit();
 });
